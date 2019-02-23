b8a3d6e870de83bf39c721ffc1a2643125a23580
skobraf <skobraf@gmail.com>
2019-02-23 15:19:01 +0000

    changed structure

--- src/components/App.js
+++ src/components/App.js
@@ -5,104 +5,39 @@ import Comment from './Comment';
 class App extends React.Component {
 	
 	state = {
-		Json:[]
-	}
-	renderForm = () => {
-		const Json = [...this.state.Json];
-		Json.push({
-			id:0,
-			visible:false,
-			name:"first name",
-			value:undefined,
-			dataType: []
-						});
-		this.setState({Json});
-	}
-
-	handlClick = (key, id, arrayIndex, json) =>  {
-		const Json = [...this.state.Json];
-		const name = json["textRef"];
-		const value = json["valueRef"];
-		const status = json["statusRef"];
-		let index = Number(id);
-		let data = Json[0];
-			for(let i = 0; i < index ; i++) {
-				data = data.dataType[0];
-			}
-			switch(status) {
-	  		case "boolean":
-				console.log("bool")					
-	    	break;
-	    	case "text":
-	    		console.log("texto")
-	    	break;
-	    	case "number":
-	    		console.log("numero");
-	    	break;
-	    	case "date":
-	    		console.log("date")
-	    	break;
-	    	case "structure":
-	    	console.log("object")
-	    	break;
-	    	case "array":
-	    	console.log("tableaus")
-	    		data.dataType.push({
-	    						id:`${index + 1}`,
-								visible:true,
-								name:name,
-								value: value,
-								dataType:[]});
-	    	break;
-	 		 default:
-		}
-
-			
-			this.setState({Json: Json})
-	}
-	getValues = (key, id, arrayIndex, json) =>  {
-		const Json = [...this.state.Json];
-		const name = json["textRef"];
-		const value = json["valueRef"];
-		const status = json["statusRef"];
-		let index = Number(id);
-		let data = Json[0];
-			for(let i = 0; i < index ; i++) {
-				data = data.dataType[0];
-			}
-			switch(status) {
-	  		case "boolean":
-				console.log("bool")					
-	    	break;
-	    	case "text":
-	    		console.log("texto")
-	    	break;
-	    	case "number":
-	    		data.dataType.push({
-	    						id:`${index + 1}`,
-								visible:true,
-								name:name,
-								value: value,
-								dataType:Number()});
-	    	break;
-	    	case "date":
-	    		console.log("date")
-	    	break;
-	 		 default:
-		}
-
-			
-			this.setState({Json: Json})
+		Json:{
+			  body :'level 1',
+			  comments: [
+				{
+					body: 'level 2',
+					comments: [
+						{
+							body: 'level 3',
+							comments: [
+								{
+									body: 'level 4',
+									comments: [
+										{
+											body: 'level 6',
+											comments: [
+											]
+										}
+									]
+								}
+							]
+						}
+					]
+				}
+				]}
 	}
 	render() {
 		return (	
 			<div>
 				<ul>
-					{this.state.Json.map((comment, i) =>
+					{Object.keys(this.state.Json).map((key, index) =>
 						<Comment 
-							Json={comment}
-							key={i}
-							id={i}
+							key={key}
+							json={this.state.Json[key]}
 							handlClick={this.handlClick}
 							getValues={this.getValues}
 						/>
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,78 +1,26 @@
 import React from 'react';
 
 class Comment extends React.Component {
-	statusRef = React.createRef();
-	textRef = React.createRef();
-	valueRef = React.createRef();
-
+	//{this.props.Json['comments'].map((comment, i) => 
+	//							<Comment 
+	//							Json={comment}
+	//							key={i}
+	//							id={i}
+	//							handlClick={this.props.handlClick}
+	//							getValues={this.props.getValues}
+	//							/>
+	//						)}
 	handleClick = () => {
-		this.setState({visible: false})
+		console.log(this.props.json)
 	}
-	handlClick = (e) => {
-		e.preventDefault();
-		const textRef = this.textRef.current.value;
-		const id = this.props.Json["id"]
-		const index = this.props.id;
-		const json = {
-			statusRef: this.statusRef.current.value,
-			textRef: this.textRef.current.value,
-			valueRef:this.valueRef.current.value
-		}
-		this.props.handlClick(textRef, id, index, json);
-	}
-		getValues = () => {
-		const textRef = this.textRef.current.value;
-		const id = this.props.Json["id"]
-		const index = this.props.id;
-		const json = {
-			statusRef: this.statusRef.current.value,
-			textRef: this.textRef.current.value,
-			valueRef:this.valueRef.current.value
-		}
-		this.props.getValues(textRef, id, index, json);
-	}
-
-	toggleVisibility = () => {
-		if (this.statusRef.current.value ==="array" || this.statusRef.current.value ==="structure" ) {
-			this.visible = true;
-		}
-		else this.visible = false;
-		
-	}
-	
 	render() {
 		let visible = false;
-		let data;
-				if(Array.isArray(this.props.Json.dataType)) {
-					data	 = 	this.props.Json.dataType.map((comment, i) => 
-								<Comment 
-								Json={comment}
-								key={i}
-								id={i}
-								handlClick={this.props.handlClick}
-								getValues={this.props.getValues}
-								/>
-							)
-				}
 		return (
 				<ul>
 					<li onClick={this.handleClick}>
-						<form onSubmit={this.handlClick}>
-							<input type="text" placeholder="Text" ref={this.textRef} />
-							<select name="valeur"  ref={this.statusRef} onChange={this.toggleVisibility}>
-								<option value="text">text</option>
-								<option value="boolean">Boolean</option>
-								<option value="number">Number</option>
-								<option value="structure">Structure</option>
-								<option value="array">Array</option>
-								<option value="date">Date</option>
-							</select>
-							<input type="text" placeholder="Value" ref={this.valueRef} onChange={this.getValues} style={{display: !this.props.visible ? 'inline-block' : 'none'}} />
-							<button  type="submit" style={{display: this.visible ? 'inline-block' : 'none'}}>add</button>
-					</form>
+						
 					</li>
 						<ul>
-							{data}
 						</ul>
 				</ul>
 			)
